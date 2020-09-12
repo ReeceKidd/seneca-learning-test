d379b8c567dfa215ddb670269c8bf422ffddd01c
Reece Kidd <reecekidd95@gmail.com>
2020-09-12 11:09:55 +0100

    refactor: Added Module model

---
+++ src/Models/Module.ts
@@ -0,0 +1,21 @@
+import * as mongoose from 'mongoose';
+
+export type ModuleModel = { modulename: string } & mongoose.Document;
+
+export const moduleSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
+        collection: 'Modules',
+    },
+);
+
+export const moduleModel: mongoose.Model<ModuleModel> = mongoose.model<ModuleModel>('Module', moduleSchema);
